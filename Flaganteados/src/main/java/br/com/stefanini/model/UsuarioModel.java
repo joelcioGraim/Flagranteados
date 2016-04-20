5ddc325263372ba66fcab7b861924aa7e85b2e23
Root <Root@10.38.100.39>
2016-04-20 11:28:17 -0300

    configurando angular.

---
+++ Flaganteados/src/main/java/br/com/stefanini/model/UsuarioModel.java
@@ -0,0 +1,71 @@
+package br.com.stefanini.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tb_usuario")
+@Entity
+public class UsuarioModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long idUsuario;
+
+	@Column
+	private String nomeUsuario;
+
+	@Column
+	private String login;
+
+	@Column
+	private String senha;
+
+	@Column(columnDefinition = "BIT", length = 1)
+	private boolean ativo;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
